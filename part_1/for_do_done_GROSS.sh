860336b4eb677ef2dd09569d5b5cef25045fb4df
Iwo Gross <ipg0004@auburn.edu>
2019-03-02 19:51:18 -0600

    Added comments and cleaned up my scripts

--- part_1/for_do_done_GROSS.sh
+++ part_1/for_do_done_GROSS.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+# This script can search multiple .txt documents for a given search term, and identifies the # of lines it is found on, and the specific line numbers.
+# The summary for each file is printed to a single .txt file.
+
+#Creating a variable for the prompted search term
 read -p "Input search term: " term
 
+#For loop starts to analyze each .txt file
 for i in ./book_pages/*.txt
 do
+# Identifies lines with $term, then removes the line number at the start of the string, and flips the list to a horizontal orientation on one line
 lines=`cat $i | grep -in $term | cut -d \: -f 1 | awk 'BEGIN { ORS = " " } { print }'`
+
+# Identifies the number of lines with $term
 count=`grep -ic "$term" $i`
 
+# Syntax conditionals to be exported in a summary .txt document.
 if [ "$count" -gt 1 ]; then
      printf "The file $i contains at least $count instances of the string '$term' on $count lines: $lines\n\n" >> output_GROSS.txt
 elif [ "$count" -eq 1 ]; then
@@ -13,4 +22,6 @@ elif [ "$count" -eq 1 ]; then
 else
     printf "The file $i contains $count instances of the string '$term'.\n\n" >> output_GROSS.txt
 fi
+
+#Done-zo
 done
--- part_1/string_finder_GROSS.sh
+++ part_1/string_finder_GROSS.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+#This script counts the numbers of lines a given search term is present on, and prints a summary statement. 
+
+#Creating file and term variables with a prompt.
 read -p "Input file name: " file
 read -p "Input search term: " term
-lines=`cat $file | grep -in $term | cut -d \: -f 1 | awk 'BEGIN { ORS = " " } { print }'`
-count=`grep -c "$term" $file`
 
+#This creates a variable called lines. It creates a string of numbers identifying lines that contain the variable $term in $file. 
+lines=`cat $file | grep -in $term | cut -d \: -f 1 | awk 'BEGIN { ORS = " " } { print }'`
+
+#Similar, but instead I am pulling the line count for all lines containing $term in $file, *none-case-sensitive*! 
+count=`grep -ic "$term" $file`
+
+#Syntax conditionals, just for readability
 if [ "${count}" -gt 1 ]; then
      printf "The file $file contains at least $count instances of the string '$term' on $count lines: $lines\n"
 elif [ "${count}" -eq 1 ]; then
