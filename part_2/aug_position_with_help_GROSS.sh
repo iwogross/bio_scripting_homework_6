f8a999f3045565c033741b62fe61d120da6d120f
Iwo Gross <ipg0004@auburn.edu>
2019-03-03 19:04:55 -0600

    Completed aug script for part_2

--- part_2/aug_position_with_help_GROSS.sh
+++ part_2/aug_position_with_help_GROSS.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 #prints three arguments in reverse order
-printf "$3 $2 $1\n"
+usage="Usage: $(basename "$0"): Command [-h] [arg1] [arg2] [arg3] -- program that prints three arguments in reverse order. Option [-h] prints this usage text."
 
-# usage=
+if [ ${#} -lt 3 ] ; then
+    printf "\n$usage\n\n"
+    exit 1;
+fi
+
+while getopts ":h" optname
+  do
+    case "$optname" in
+      "h")
+        printf "\n$usage\n\n"
+        exit 0;
+        ;;
+        *)
+        printf "\nError: Unknown option: -$OPTARG\n"
+        exit 0;
+        ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+printf "$3 $2 $1\n"
