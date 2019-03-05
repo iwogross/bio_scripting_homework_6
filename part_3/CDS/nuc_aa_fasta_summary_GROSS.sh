766a86b4438e98da8d88549b0f538b6da782d4e4
Iwo Gross <ipg0004@auburn.edu>
2019-03-04 19:21:27 -0600

    Added comments to both scripts

--- part_3/CDS/nuc_aa_fasta_summary_GROSS.sh
+++ part_3/CDS/nuc_aa_fasta_summary_GROSS.sh
@@ -3,14 +3,20 @@
 for i in *.cds
 do
 basename=`basename ${i} .cds`
+
+# Script for FASTA descriptor truncation and complete summarization (with histogram)
 cat ${i} | cut -d ' ' -f 1 | ./get_fasta_stats.pl -g -T > ${basename}_GROSS.dna
+# Solo truncation pipe to prepare for transeq
 cat ${i} | cut -d ' ' -f 1 > ${basename}_temp_GROSS.dna
+
+# Translates temp.dna FASTA code with truncated descriptors into amino acid sequence
 transeq -sformat pearson -frame 1 -sequence ${basename}_temp_GROSS.dna -outseq ${basename}_GROSS.pep
 done
 
 for i in *.pep
 do
 base=`basename ${i} _GROSS.pep`
+# This loop takes previous .pep file and creates a full FASTA summary and a per-contig summary
 ./get_fasta_stats.pl -t ${base}_GROSS.pep > ${base}_GROSS.summary
 done
 
--- part_3/CDS/test_GROSS.sh
+++ part_3/CDS/test_GROSS.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Script that clips fasta descriptor names and produces complete FASTA statistics with a histogram
+# I sort of combined 3a and 5 script here. 
+# Next, transeq translates the sequences at the first reading frame 
+
 cat $1 | cut -d ' ' -f 1 | ./get_fasta_stats.pl -g -T > output_GROSS.dna
 cat $1 | cut -d ' ' -f 1 > output_temp_GROSS.dna
 transeq -sformat pearson -frame 1 -sequence output_temp_GROSS.dna -outseq output_GROSS.pep
 
+# Finally, a second complete FASTA summary with individual stats for each contig
 ./get_fasta_stats.pl -t output_GROSS.pep > output_GROSS.summary
 
 
